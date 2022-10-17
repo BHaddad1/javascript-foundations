022ce26cd9c3472775a6950d89e07576a4a41813
Blanche Haddad <blancherachel@gmail.com>
2022-10-17 07:45:01 -0600

    Initial commit

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name,
+    this.rider = rider,
+    this.hungry = true
+    this.ate = 0
+  }
+  greet(){
+    var rider = this.rider
+    return `Hi, ${rider}!`
+  }
+  eat(){
+    this.ate++
+    if(this.ate > 2){
+      return this.hungry = false
+    }
+  }
+}
 
+module.exports = Dragon;
