ba0434e1190f66f5d2e9e20202400ebf8809edc7
Blanche Haddad <blancherachel@gmail.com>
2022-10-17 19:55:30 -0600

    Save work in Hobbit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,16 +1,31 @@
 class Hobbit {
   constructor({name}){
-    this.name = name
-    this.age = 0
-    this.adult = false
+    this.name = name,
+    this.age = 0,
+    this.adult = false,
+    this.old = false,
+    this.hasRIng = null
   }
+
   celebrateBirthday(){
     this.age++
     if (this.age <= 32){
     this.adult = false
+  } else if (this.age >= 101){
+    this.old = true
   } else {
     this.adult = true
   }
+  }
+
+  getRing(){
+    if (this.name === "Frodo"){
+      this.hasRing = true
+      return `Here is the ring!`
+    } else {
+      this.hasRing = false
+      return 'You can\'t have it!'
+    }
   }
 
 }
