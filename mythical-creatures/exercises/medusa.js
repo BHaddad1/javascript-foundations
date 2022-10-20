55997718cf2bbac5ce28e89eff037e2f21e01456
Blanche Haddad <blancherachel@gmail.com>
2022-10-20 15:52:08 -0600

    Save work in Medusa babyyyyyyy

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,28 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person1 = require('./person');
+var Statue1 = require('./statue');
+var Medusa1 = require('./medusa');
+
+class Medusa {
+  constructor(name){
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(newVictim){
+    var newStatue = new Statue(newVictim.name)
+    this.statues.push(newStatue);
+    if(this.statues.length === 4){
+    var freedPerson = this.statues.shift()
+    var newPerson = new Person(freedPerson.name)
+    newPerson.mood = 'relieved'
+    return newPerson
+  }
+
+  }
+
+}
+
+
+var Statue = require('../exercises/statue');
+var Person = require('../exercises/person');
+
+module.exports = Medusa;
---
+++ mythical-creatures/exercises/person.js
@@ -0,0 +1,10 @@
+class Person {
+  constructor(name, mood){
+    this.name = name;
+    this.mood = 'frightened';
+  }
+}
+
+
+
+module.exports = Person;
